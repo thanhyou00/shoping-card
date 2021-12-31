4b0b9e9369e99f4c6f060ae81c7fa234af523313
thanhyou00 <truc06082000@gmail.com>
2021-12-31 09:30:55 +0700

    thanhyou00

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { BodyComponent } from './body/body.component';
 import { FooterComponent } from './footer/footer.component';
+import { CardSummaryComponent } from './card-summary/card-summary.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { PromoCodeComponent } from './promo-code/promo-code.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     BodyComponent,
-    FooterComponent
+    FooterComponent,
+    CardSummaryComponent,
+    ProductListComponent,
+    PromoCodeComponent
   ],
   imports: [
     BrowserModule,
