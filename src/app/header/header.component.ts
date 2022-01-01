b2527f1412f7339a8b1a5989d6307131a0b2fc35
thanhyou00 <truc06082000@gmail.com>
2022-01-01 07:00:29 +0700

    thanhyou00

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   title = 'Shoping Card';
+  numberItems : number = 4;
 
   constructor() { }
 
--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../product.model';
+
 
 @Component({
   selector: 'app-product-list',
@@ -7,6 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductListComponent implements OnInit {
 
+  products : Product[] = [
+    {
+      id : '1',
+      name : 'iPhone X 64GB ',
+      description : 'Description for product item number 1',
+      thumbnail : '../../assets/images/iphone_x_64gb_3.jpg',
+      price : 115.99,
+      quantity : 2
+    },
+    {
+      id : '2',
+      name : 'Apple iPhone 13 256GB Pink',
+      description : 'Description for product item number 2',
+      thumbnail : '../../assets/images/Pink.png',
+      price : 15.99,
+      quantity : 2
+    }
+  ];
   constructor() { }
 
   ngOnInit(): void {
